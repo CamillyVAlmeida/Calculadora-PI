fe065b4de1619f23f65cb4abd19205b3224f4c52
Camilly Vitoria Almeida <camilly.almeidavs@gmail.com>
2025-06-22 22:07:10 -0300

    novo teste unitário para realização de merge

--- PI/src/test/java/Calculadora/UnitarioTest.java
+++ PI/src/test/java/Calculadora/UnitarioTest.java
@@ -5,12 +5,12 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 public class UnitarioTest {
-    
+
     Calculadora calc = new Calculadora();
 
     @Test
     void testSomar() {
-        assertEquals(10.0, calc.somar(6, 4 ), 0.0001);
+        assertEquals(4.0, calc.somar(2, 2 ), 0.0001);
     }
     
 }
